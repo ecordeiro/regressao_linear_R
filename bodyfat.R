5224d74ce4c8312a19cb0ffb72aa82de33bb0df3
Emmanuel Cordeiro <ecordeiro@gmail.com>
2023-02-15 20:51:24 -0300

    Arquivos
    Novos

---
+++ bodyfat.R
@@ -0,0 +1,19 @@
+#Vamos começar a brincadeira#####################
+
+#1. Carregando Pacotes#####
+library(readr)
+library(openxlsx) #biblioteca para escrever arquivo em excel
+library(haven)
+library(readxl)
+library(tidyverse)
+library(yardstick) #biblioteca para calcular medidas de erro
+library(lmtest) # calcula o teste de homogeneidade de variancia
+library(car) # calcula vif
+library(ggraph)
+library(plotly)
+library(ggstance)
+library(jtools)
+library(olsrr)
+library(PerformanceAnalytics)
+library(correlation)
+
